0e98d025f4249b9bdecfcbf804916cd1ec436129
Serge Kotlin <jobsfolder@yandex.ru>
2021-11-03 03:31:49 +0300

    try of converting 2 classes (Home Work 1)

--- app/src/main/java/com/android1/android1_notes/MainFragment.kt
+++ app/src/main/java/com/android1/android1_notes/MainFragment.kt
@@ -1,63 +1,48 @@
-package com.android1.android1_notes;
+package com.android1.android1_notes
 
-import android.annotation.SuppressLint;
-import android.content.res.Configuration;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.annotation.SuppressLint
+import android.content.res.Configuration
+import android.graphics.Color
+import android.os.Bundle
+import android.view.*
+import android.view.ContextMenu.ContextMenuInfo
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import java.util.*
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+class MainFragment : Fragment() {
+    private var currentNote // Выбранная заметка
+            : Note? = null
+    private var isLandscape = false
 
-import java.util.Random;
-
-public class MainFragment extends Fragment {
-
-    public static final String CURRENT_NOTE = "CurrentNote";
-    private Note currentNote;    // Выбранная заметка
-    private boolean isLandscape;
-
-//    @Override
-//    public void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
-
+    //    @Override
+    //    public void onCreate(@Nullable Bundle savedInstanceState) {
+    //        super.onCreate(savedInstanceState);
+    //    }
     // Сохраним текущую позицию (вызывается перед выходом из фрагмента)
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putParcelable(CURRENT_NOTE, currentNote);
-        super.onSaveInstanceState(outState);
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putParcelable(CURRENT_NOTE, currentNote)
+        super.onSaveInstanceState(outState)
     }
 
     //TODO Почему в этом фрагменте этот метод is deprecated, когда в другом всё нормально
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
         // Определение, можно ли будет открыть рядом заметку в другом фрагменте
-        isLandscape = getResources().getConfiguration().orientation
-                == Configuration.ORIENTATION_LANDSCAPE;
+        isLandscape = (resources.configuration.orientation
+                == Configuration.ORIENTATION_LANDSCAPE)
 
         // Если это не первое создание, то восстановим текущую позицию
-        if (savedInstanceState != null) {
+        currentNote = if (savedInstanceState != null) {
             // Восстановление текущей позиции.
-            currentNote = savedInstanceState.getParcelable(CURRENT_NOTE);
+            savedInstanceState.getParcelable(CURRENT_NOTE)
         } else {
             // Если восстановить не удалось, можно показать объект с первым индексом
-            currentNote = new Note(0, getResources().getStringArray(R.array.notes)[0]);
+            Note(0, resources.getStringArray(R.array.notes)[0])
         }
 
         //TODO Не работает. Не получается организовать верную передачу и обработку заметки между ориентациями
@@ -66,147 +51,147 @@ public class MainFragment extends Fragment {
         // showNote(currentNote);
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-        setHasOptionsMenu(true); // Регестрируем меню! Не забываем
-        return view;
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        setHasOptionsMenu(true) // Регестрируем меню! Не забываем
+        return view
     }
 
     // Вызывается после создания макета фрагмента
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        initList(view); // проинициализируем список заметок
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initList(view) // проинициализируем список заметок
     }
 
     // Создаём список городов на экране из массива в ресурсах
-    private void initList(View view) {
-        LinearLayout layoutView = (LinearLayout)view;
-        String[] notes = getResources().getStringArray(R.array.notes_names);
-
-        for(int i=0; i < notes.length; i++){
-            String note = notes[i];
-            TextView tv = new TextView(getContext());
-            tv.setText(note);
-            tv.setTextSize(30);
-
-            setNoteColor(tv);
-
-            layoutView.addView(tv);
-
-            final int fi = i; // не можем внутрь анонимного класса передать не final - иначе гонка потоков
-            tv.setOnClickListener(v -> {
-                currentNote = new Note(fi, getResources().getStringArray(R.array.notes)[fi]);
-                showNote(currentNote);
-            });
-
-            registerForContextMenu(tv); // Для контекстного меню регистрируем таргет
+    private fun initList(view: View) {
+        val layoutView = view as LinearLayout
+        val notes = resources.getStringArray(R.array.notes_names)
+        for (i in notes.indices) {
+            val note = notes[i]
+            val tv = TextView(context)
+            tv.text = note
+            tv.textSize = 30f
+            setNoteColor(tv)
+            layoutView.addView(tv)
+            val fi: Int = i // не можем внутрь анонимного класса передать не final - иначе гонка потоков
+            tv.setOnClickListener { v: View? ->
+                currentNote = Note(fi, resources.getStringArray(R.array.notes)[fi])
+                showNote(currentNote!!)
+            }
+            registerForContextMenu(tv) // Для контекстного меню регистрируем таргет
         }
     }
 
     //TODO #1_Not_important ИСКЛЮЧИТЬ ПОВТОР ЦВЕТОВ ДРУГ ЗА ДРУГОМ
-    private void setNoteColor(TextView tv) {
-        String[] notes_colors = getResources().getStringArray(R.array.notes_colors);
-        int note_color;
-        int size_colors_arr = notes_colors.length;
-        if (size_colors_arr <= 0) {
-            note_color = R.color.yellow_note;
+    private fun setNoteColor(tv: TextView) {
+        val notes_colors = resources.getStringArray(R.array.notes_colors)
+        val note_color: Int
+        val size_colors_arr = notes_colors.size
+        note_color = if (size_colors_arr <= 0) {
+            R.color.yellow_note
         } else {
-            Random random = new Random();
-            int ind_note_color = random.nextInt(size_colors_arr);
-            note_color = Color.parseColor(notes_colors[ind_note_color]);
+            val random = Random()
+            val ind_note_color = random.nextInt(size_colors_arr)
+            Color.parseColor(notes_colors[ind_note_color])
         }
-        tv.setTextColor(note_color);
+        tv.setTextColor(note_color)
     }
 
-    private void showNote(Note currentNote) {
-        NoteFragment detail = NoteFragment.newInstance(currentNote); // Создаём новый фрагмент с текущей позицией для открытия заметки
-        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager(); // Выполняем транзакцию по замене фрагмента
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.addToBackStack(null); // Перед показом заметки закинем лист заметок, с которого переходим, в БэкСтэк
-        replacingFragment(detail, fragmentTransaction);
+    private fun showNote(currentNote: Note) {
+        val detail = NoteFragment.newInstance(currentNote) // Создаём новый фрагмент с текущей позицией для открытия заметки
+        val fragmentManager = requireActivity().supportFragmentManager // Выполняем транзакцию по замене фрагмента
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.addToBackStack(null) // Перед показом заметки закинем лист заметок, с которого переходим, в БэкСтэк
+        replacingFragment(detail, fragmentTransaction)
         // fragmentTransaction.add(R.id.notes, detail); // TODO сделать мульти-оконное открытие заметок (опционально, по выбору).
         // Такого не видел у конкурентов. P.s. в некоторых ситуациях - определенно удобная штука.
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        fragmentTransaction.commit();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        fragmentTransaction.commit()
     }
 
-    private void replacingFragment(NoteFragment detail, FragmentTransaction fragmentTransaction) {
-        isLandscape = getResources().getConfiguration().orientation
-                == Configuration.ORIENTATION_LANDSCAPE;
+    private fun replacingFragment(detail: NoteFragment, fragmentTransaction: FragmentTransaction) {
+        isLandscape = (resources.configuration.orientation
+                == Configuration.ORIENTATION_LANDSCAPE)
         if (isLandscape) {
-            fragmentTransaction.replace(R.id.note_container, detail);
+            fragmentTransaction.replace(R.id.note_container, detail)
         } else {
-            fragmentTransaction.replace(R.id.fragment_container, detail);  // Замена фрагмента
+            fragmentTransaction.replace(R.id.fragment_container, detail) // Замена фрагмента
         }
     }
 
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.main_menu, menu);
-    }
-
-    @Override @SuppressLint("NonConstantResourceId")
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        //TODO На ландщафтной ориентации добавленные меню дублируются, так как их фрагмент остаётся присутствовать на экране
-        // Постоянно "пересоздаётся" экран/фрагмент при выборе меню
-        // К тому же, фрагмент для списка заметок будто добавляется дважды, тогда это мешает и корректному SaveInstance
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.notes_view_choice__main_menu:
-                toastOnOptionsItemSelected("Выбор вида представления");
-                return true;
-            //TODO Сортировка должна быть здесь, на main фрагменте, но toasts срабатывают лишь на активити
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void toastOnOptionsItemSelected(CharSequence text) {
-        Toast toast = Toast.makeText(getContext(),
-                text, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.END, 0, 0);
-        toast.show();
-    }
-
-    @Override
-    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        requireActivity().getMenuInflater().inflate(R.menu.context_main, menu);
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
     }
 
     @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onContextItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.copy_note__context_main:
-                toastOnOptionsItemSelected("Заметка скопирована");
-                return true;
-            case R.id.share_note__context_main:
-                toastOnOptionsItemSelected("Заметка передана / Открыта через..");
-                return true;
-            case R.id.new_label__context_main:
-                toastOnOptionsItemSelected("Добавлена новая метка");
-                return true;
-            case R.id.pin_to_top__context_main:
-                toastOnOptionsItemSelected("Заметка закреплена");
-                return true;
-            case R.id.search__context_main:
-                toastOnOptionsItemSelected("Поиск в заметке");
-                return true;
-            case R.id.info__context_main:
-                toastOnOptionsItemSelected("Детали заметки");
-                return true;
-            case R.id.rename__context_main:
-                toastOnOptionsItemSelected("Заметка переименована");
-                return true;
-            case R.id.delete__context_main:
-                toastOnOptionsItemSelected("Заметка удалена");
-                return true;
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //TODO На ландщафтной ориентации добавленные меню дублируются, так как их фрагмент остаётся присутствовать на экране
+        // Постоянно "пересоздаётся" экран/фрагмент при выборе меню
+        // К тому же, фрагмент для списка заметок будто добавляется дважды, тогда это мешает и корректному SaveInstance
+        val id = item.itemId
+        when (id) {
+            R.id.notes_view_choice__main_menu -> {
+                toastOnOptionsItemSelected("Выбор вида представления")
+                return true
+            }
         }
-        return super.onContextItemSelected(item);
+        return super.onOptionsItemSelected(item)
     }
 
+    private fun toastOnOptionsItemSelected(text: CharSequence) {
+        val toast = Toast.makeText(context,
+                text, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.END, 0, 0)
+        toast.show()
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        requireActivity().menuInflater.inflate(R.menu.context_main, menu)
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            R.id.copy_note__context_main -> {
+                toastOnOptionsItemSelected("Заметка скопирована")
+                return true
+            }
+            R.id.share_note__context_main -> {
+                toastOnOptionsItemSelected("Заметка передана / Открыта через..")
+                return true
+            }
+            R.id.new_label__context_main -> {
+                toastOnOptionsItemSelected("Добавлена новая метка")
+                return true
+            }
+            R.id.pin_to_top__context_main -> {
+                toastOnOptionsItemSelected("Заметка закреплена")
+                return true
+            }
+            R.id.search__context_main -> {
+                toastOnOptionsItemSelected("Поиск в заметке")
+                return true
+            }
+            R.id.info__context_main -> {
+                toastOnOptionsItemSelected("Детали заметки")
+                return true
+            }
+            R.id.rename__context_main -> {
+                toastOnOptionsItemSelected("Заметка переименована")
+                return true
+            }
+            R.id.delete__context_main -> {
+                toastOnOptionsItemSelected("Заметка удалена")
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    companion object {
+        const val CURRENT_NOTE = "CurrentNote"
+    }
 }
--- app/src/main/java/com/android1/android1_notes/SettingsFragment.kt
+++ app/src/main/java/com/android1/android1_notes/SettingsFragment.kt
@@ -1,74 +1,67 @@
-package com.android1.android1_notes;
+package com.android1.android1_notes
 
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RadioButton;
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CompoundButton
+import android.widget.RadioButton
+import androidx.appcompat.widget.SwitchCompat
+import androidx.fragment.app.Fragment
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.SwitchCompat;
-import androidx.fragment.app.Fragment;
-
-import static android.content.Context.MODE_PRIVATE;
-
-public class SettingsFragment extends Fragment {
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_settings, container, false);
-        initView(view);
-        return view;
+class SettingsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        initView(view)
+        return view
     }
 
-    private void initView(View view) {
-        initRadioBackStack(view);
-        initRadioBackAsRemove(view);
-        initSwitchBlackTheme(view);
+    private fun initView(view: View) {
+        initRadioBackStack(view)
+        initRadioBackAsRemove(view)
+        initSwitchBlackTheme(view)
     }
 
-    private void initRadioBackStack(View view) {
-        RadioButton radioButtonUseBackStack = view.findViewById(R.id.radioButtonUseBackstack);
-        radioButtonUseBackStack.setChecked(Settings.isBackStack);
-        radioButtonUseBackStack.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            Settings.isBackStack = isChecked;
-            writeSettings();
-        });
+    private fun initRadioBackStack(view: View) {
+        val radioButtonUseBackStack = view.findViewById<RadioButton>(R.id.radioButtonUseBackstack)
+        radioButtonUseBackStack.isChecked = Settings.isBackStack
+        radioButtonUseBackStack.setOnCheckedChangeListener { buttonView: CompoundButton?, isChecked: Boolean ->
+            Settings.isBackStack = isChecked
+            writeSettings()
+        }
     }
 
-    private void initRadioBackAsRemove(View view) {
+    private fun initRadioBackAsRemove(view: View) {
         // Как можно заметить - инвертный выбор "isBackStack"
-        RadioButton radioButtonBackAsRemove = view.findViewById(R.id.radioButtonBackAsRemove);
-        radioButtonBackAsRemove.setChecked(!Settings.isBackStack);
-        radioButtonBackAsRemove.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            Settings.isBackStack = !isChecked;
-            writeSettings();
-        });
+        val radioButtonBackAsRemove = view.findViewById<RadioButton>(R.id.radioButtonBackAsRemove)
+        radioButtonBackAsRemove.isChecked = !Settings.isBackStack
+        radioButtonBackAsRemove.setOnCheckedChangeListener { buttonView: CompoundButton?, isChecked: Boolean ->
+            Settings.isBackStack = !isChecked
+            writeSettings()
+        }
     }
 
-    private void initSwitchBlackTheme(View view) {
-        SwitchCompat switchBlackTheme = view.findViewById(R.id.switchBlackTheme);
-        switchBlackTheme.setChecked(Settings.isBlackTheme);
-        switchBlackTheme.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            Settings.isBlackTheme = isChecked;
+    private fun initSwitchBlackTheme(view: View) {
+        val switchBlackTheme: SwitchCompat = view.findViewById(R.id.switchBlackTheme)
+        switchBlackTheme.isChecked = Settings.isBlackTheme
+        switchBlackTheme.setOnCheckedChangeListener { buttonView: CompoundButton?, isChecked: Boolean ->
+            Settings.isBlackTheme = isChecked
             //TODO setTheme();
-            writeSettings();
-        });
+            writeSettings()
+        }
     }
 
-//    private void setTheme() {
-//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-//        getActivity().recreate();
-//    }
-
+    //    private void setTheme() {
+    //        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    //        getActivity().recreate();
+    //    }
     // Сохранение настроек приложения
-    private void writeSettings(){
-        SharedPreferences sharedPref = requireActivity().getSharedPreferences(Settings.SHARED_PREFERENCE_NAME, MODE_PRIVATE); // Специальный класс для хранения настроек
-        SharedPreferences.Editor editor = sharedPref.edit(); // Настройки сохраняются посредством специального класса editor
-        editor.putBoolean(Settings.IS_BACK_STACK_USED, Settings.isBackStack); // Задаём значения настроек
-        editor.putBoolean(Settings.IS_BLACK_THEME_USED, Settings.isBlackTheme);
-        editor.apply(); // Сохраняем значения настроек
+    private fun writeSettings() {
+        val sharedPref = requireActivity().getSharedPreferences(Settings.SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE) // Специальный класс для хранения настроек
+        val editor = sharedPref.edit() // Настройки сохраняются посредством специального класса editor
+        editor.putBoolean(Settings.IS_BACK_STACK_USED, Settings.isBackStack) // Задаём значения настроек
+        editor.putBoolean(Settings.IS_BLACK_THEME_USED, Settings.isBlackTheme)
+        editor.apply() // Сохраняем значения настроек
     }
 }
